28ba598e63113bdc26b084983d1b26c31a711e0b
Romnan Stachura <stachrom@gmail.com>
2015-03-23 23:35:09 +0100

    trainer role and admin dashboard

--- server/roles.js
+++ server/roles.js
@@ -2,14 +2,17 @@
 
 Meteor.startup(function () {
 	
-   //Roles.createRole("admin");
-   var role = ['admin'];
+   // Roles.createRole("admin");
+   // Roles.createRole("trainer");
+   var role = ['admin', 'trainer'];
 
    //console.log(role); 
  
    var roles = Roles.getAllRoles();
- 
-   // Roles.addUsersToRoles('xwiWMCagf5FWghCS8', role );
+
+   // http://alanning.github.io/meteor-roles/classes/Roles.html#method_setUserRoles
+   // Roles.setUserRoles([user1, user2], ['trainer'])
+   Roles.addUsersToRoles('xwiWMCagf5FWghCS8', role );
    Roles.addUsersToRoles('RP3LFTcYJrvzcdEAA', role );
    Roles.addUsersToRoles('q29enr9d3uLXqwCEk', role );
    Roles.addUsersToRoles('EhkpjeHDiZqFkkkCq', role );
